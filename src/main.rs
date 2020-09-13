a58ec1257a823a0e3e28c25f4d665ea752f95587
Caleb Boylan <calebboylan@gmail.com>
2020-09-12 17:24:14 -0700

    Add SERVER_URL functionality

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,12 @@ use tokio_diesel::*;
 pub mod models;
 pub mod schema;
 
+#[derive(Clone)]
+struct ServerConfig {
+    url: String,
+}
+
 pub fn establish_connection() -> diesel::r2d2::Pool<diesel::r2d2::ConnectionManager<PgConnection>> {
-    dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let manager = ConnectionManager::<PgConnection>::new(database_url);
     let pool = Pool::builder()
@@ -27,10 +31,21 @@ pub fn establish_connection() -> diesel::r2d2::Pool<diesel::r2d2::ConnectionMana
     return pool;
 }
 
-async fn create_shortened_link(db: web::Data<Pool<diesel::r2d2::ConnectionManager<PgConnection>>>, params: Json<models::LinkCreate>) -> impl Responder {
+async fn create_shortened_link(config: web::Data<ServerConfig>, db: web::Data<Pool<diesel::r2d2::ConnectionManager<PgConnection>>>, params: Json<models::LinkCreate>) -> impl Responder {
     use schema::links::dsl::*;
+    #[derive(Serialize)]
+    struct Response {
+        url: String,
+    };
+    let mut url = config.url.clone();
     let result: models::Link = diesel::insert_into(links).values(params.into_inner()).get_result_async(&db).await.expect("failed to insert ");
-    HttpResponse::Ok().body(serde_json::to_string(&result).expect("failed to serialize"))
+
+    url.push_str(&result.id);
+
+    let response = Response {
+        url,
+    };
+    HttpResponse::Ok().body(serde_json::to_string(&response).expect("failed to serialize"))
 }
 
 async fn get_all_links(db: web::Data<Pool<diesel::r2d2::ConnectionManager<PgConnection>>>) -> impl Responder {
@@ -48,10 +63,19 @@ async fn redirect(db: web::Data<Pool<diesel::r2d2::ConnectionManager<PgConnectio
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
     let pool = establish_connection();
 
+    let url = env::var("SERVER_URL").expect("SERVER_URL must be set");
+
+    let config = ServerConfig {
+        url,
+    };
+
     HttpServer::new(move ||
-        App::new().data(pool.clone())
+        App::new()
+            .data(pool.clone())
+            .data(config.clone())
             .route("/", web::post().to(create_shortened_link))
             .route("/", web::get().to(get_all_links))
             .route("/{id}", web::get().to(redirect)))
